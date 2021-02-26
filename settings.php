a5c445ba865eb5d3e5e07616d235e583a8acbf73
genbtcdoubler <79389495+genbtcdoubler@users.noreply.github.com>
2021-02-26 01:49:34 -0800

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,9 +1,9 @@
 <?php
 
-$x_install_uuid = "xxx";
+$x_install_uuid = "f29ac4e8-80f4-4b67-b317-ca3432a6709c";
 
-$user_agent = "xxx";
+$user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36";
 
-$refresh_token = "xxx";
+$refresh_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOiIxNjE2OTIzODY1IiwidXNlcklkIjoyNDA3NTM4MywiaW5zdGFsbFV1aWQiOiJmMjlhYzRlOC04MGY0LTRiNjctYjMxNy1jYTM0MzJhNjcwOWMiLCJ0eXBlIjoicmVmcmVzaFRva2VuIiwiYXV0aG9yaXphdGlvbkxldmVsIjoiZnVsbCIsInBsYXRmb3JtIjoid2ViIn0.7TWxBwARayfMryi6kIQFFoW5pxOuhUf79GqYexhFn5M";
 
 ?>
